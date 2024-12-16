0fa070dc08cb37c4b3984bc353b8ecdd066f1b5d
marcoclase <47568193+marcoclase@users.noreply.github.com>
2024-12-16 13:54:14 +0100

    Update ej16.sh

--- ej16.sh
+++ ej16.sh
@@ -2,12 +2,11 @@
 #Realiza un script donde el usuario debe adivinar un número
 #al azar generado por la computadora.
 
-#Se genera un número aleatorio del 1 al 10
-AZAR=$[$RANDOM%10+1]
+#Se genera un número aleatorio del 1 al 100
+AZAR=$[$RANDOM%100+1]
 while [ 1 ]
 do
-   echo –n "Ingrese un número: "
-   read NUM
+   read -p "Ingrese un número:" NUM
    if [ $NUM -eq $AZAR ]
    then
       echo "Acertaste!"
