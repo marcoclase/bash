76c86688a57f78358637f1066669c80d9802fd55
marcoclase <47568193+marcoclase@users.noreply.github.com>
2024-12-16 11:56:23 +0100

    Add files via upload

---
+++ ej01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Máquina:" $HOSTNAME
+echo "Usuaio:" $USER
+echo "Identificador de usuario (PID):" $UID
+echo "Identificador de grupo:" $GROUPS
+echo "Directorio personal:" $HOME
+echo "Directorio de trabajo:" $PWD
